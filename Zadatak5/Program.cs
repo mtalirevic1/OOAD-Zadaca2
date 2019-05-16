94dc4db0d77c7d0677e78cf4aeca31715dfb05dc
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2019-05-17 01:29:05 +0200

    Zadatak 5

---
+++ Zadatak5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zadatak5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractFactory shapeFactory = FactoryProducer.getFactory(false);
+            Sveska sveska1 = shapeFactory.getSveska("kockice");
+            sveska1.Napravi();
+            Sveska sveska2 = shapeFactory.getSveska("linije");
+            sveska2.Napravi();
+            AbstractFactory shapeFactory1 = FactoryProducer.getFactory(true);
+            Sveska sveska3 = shapeFactory1.getSveska("kockice");
+            sveska3.Napravi();
+            Sveska sveska4= shapeFactory1.getSveska("linije");
+            sveska4.Napravi();
+        }
+    }
+}
